0123889d3e969ece8736b3318c194c340d211807
AscaL <emanuele.orfanelli@fwr.it>
2015-05-31 03:28:06 +0200

    add semicolons

--- api/controllers/AclController.js
+++ api/controllers/AclController.js
@@ -8,178 +8,174 @@
 
 module.exports = {
 
-	allow: function(req, res) {
-		var roles = safeLowerCase(req.param('roles'))
-		var controller = safeLowerCase(req.param('controller'))
-		var actions = safeLowerCase(req.param('actions'))
+	allow: function (req, res) {
+		var roles = safeLowerCase(req.param('roles'));
+		var controller = safeLowerCase(req.param('controller'));
+		var actions = safeLowerCase(req.param('actions'));
 
-		var paramsError = utility.checkParams(req, 'roles', 'controller', 'actions')
+		var paramsError = utility.checkParams(req, 'roles', 'controller', 'actions');
+
+		if (paramsError) {
+			return res.badRequest(paramsError);
+		}
+
+		acl.allow(roles, controller, actions, function (err) {
+			if (err) {
+				winston.error(err);
+				return res.serverError(err);
+			} else {
+				return res.ok();
+			}
+		});
+	},
+
+	revoke: function (req, res) {
+		var roles = safeLowerCase(req.param('roles'));
+		var controller = safeLowerCase(req.param('controller'));
+		var actions = safeLowerCase(req.param('actions'));
+
+		var paramsError = utility.checkParams(req, 'roles', 'controller', 'actions');
 
 		if (paramsError) {
 			return res.badRequest(paramsError)
 		}
 
-		acl.allow(roles, controller, actions, function(err) {
+		acl.removeAllow(roles, controller, actions, function (err) {
 			if (err) {
-				winston.error(err)
-				return res.serverError(err)
+				winston.error(err);
+				return res.serverError(err);
+			} else {
+				return res.ok();
 			}
-			else {
-				return res.ok()
-			}
-		})
+		});
 	},
 
-	revoke: function(req, res) {
-		var roles = safeLowerCase(req.param('roles'))
-		var controller = safeLowerCase(req.param('controller'))
-		var actions = safeLowerCase(req.param('actions'))
+	addUserRoles: function (req, res) {
+		var userId = req.param('id');
+		var roles = safeLowerCase(req.param('roles'));
 
-		var paramsError = utility.checkParams(req, 'roles', 'controller', 'actions')
+		var paramsError = utility.checkParams(req, 'id', 'roles');
 
 		if (paramsError) {
-			return res.badRequest(paramsError)
+			return res.badRequest(paramsError);
 		}
 
-		acl.removeAllow(roles, controller, actions, function(err) {
+		acl.addUserRoles(userId, roles, function (err) {
 			if (err) {
-				winston.error(err)
-				return res.serverError(err)
+				winston.error(err);
+				return res.serverError(err);
+			} else {
+				return res.ok();
 			}
-			else {
-				return res.ok()
-			}
-		})
+		});
 	},
 
-	addUserRoles: function(req, res) {
+	getUserRoles: function (req, res) {
+		var userId = req.param('id');
+
+		var paramsError = utility.checkParams(req, 'id');
+
+		if (paramsError) {
+			return res.badRequest(paramsError);
+		}
+
+		acl.userRoles(userId, function (err, roles) {
+			if (err) {
+				winston.error(err);
+				return res.serverError(err);
+			}
+			return res.json(roles);
+		});
+	},
+
+	getAllRoles: function (req, res) {
+		acl.userRoles('*', function (err, roles) {
+			if (err) {
+				winston.error(err);
+				return res.serverError(err);
+			}
+			return res.json(roles);
+		});
+	},
+
+	removeUserRoles: function (req, res) {
 		var userId = req.param('id')
-		var roles = safeLowerCase(req.param('roles'))
+		var roles = safeLowerCase(req.param('roles'));
 
-		var paramsError = utility.checkParams(req, 'id', 'roles')
+		var paramsError = utility.checkParams(req, 'id', 'roles');
 
 		if (paramsError) {
-			return res.badRequest(paramsError)
+			return res.badRequest(paramsError);
 		}
 
-		acl.addUserRoles(userId, roles, function(err) {
+		acl.removeUserRoles(userId, roles, function (err) {
 			if (err) {
-				winston.error(err)
-				return res.serverError(err)
+				winston.error(err);
+				return res.serverError(err);
+			} else {
+				return res.ok();
 			}
-			else {
-				return res.ok()
-			}
-		})
+		});
 	},
 
-	getUserRoles: function(req, res) {
-		var userId = req.param('id')
+	removeRole: function (req, res) {
+		var role = safeLowerCase(req.param('role'));
 
-		var paramsError = utility.checkParams(req, 'id')
+		var paramsError = utility.checkParams(req, 'role');
 
 		if (paramsError) {
-			return res.badRequest(paramsError)
+			return res.badRequest(paramsError);
 		}
 
-    acl.userRoles(userId, function(err, roles) {
-    	if(err) {
-    		winston.error(err)
-    		return res.serverError(err)
-    	}
-    	return res.json(roles)
-    })
-	},
-
-	getAllRoles: function(req, res) {
-    acl.userRoles('*', function(err, roles) {
-    	if(err) {
-    		winston.error(err)
-    		return res.serverError(err)
-    	}
-    	return res.json(roles)
-    })
-	},
-
-	removeUserRoles: function(req, res) {
-		var userId = req.param('id')
-		var roles = safeLowerCase(req.param('roles'))
-
-		var paramsError = utility.checkParams(req, 'id', 'roles')
-
-		if (paramsError) {
-			return res.badRequest(paramsError)
-		}
-
-		acl.removeUserRoles(userId, roles, function(err) {
+		acl.removeRole(role, function (err) {
 			if (err) {
-				winston.error(err)
-				return res.serverError(err)
+				winston.error(err);
+				return res.serverError(err);
+			} else {
+				return res.ok();
 			}
-			else return res.ok()
-		})
+		});
 	},
 
-	removeRole: function(req, res) {
-		var role = safeLowerCase(req.param('role'))
+	getControllers: function (req, res) {
+		return res.json(utility.getAllControllers());
+	},
 
-		var paramsError = utility.checkParams(req, 'role')
+	whatResources: function (req, res) {
+		var roles = safeLowerCase(req.param('roles'));
+
+		var paramsError = utility.checkParams(req, 'roles');
 
 		if (paramsError) {
-			return res.badRequest(paramsError)
+			return res.badRequest(paramsError);
 		}
 
-		acl.removeRole(role, function(err) {
+		acl.whatResources(roles, function (err, result) {
 			if (err) {
-				winston.error(err)
-				return res.serverError(err)
+				winston.error(err);
+				return res.serverError(err);
+			} else {
+				return res.json(result);
 			}
-			else {
-				return res.ok()
-			}
-		})
-	},
-
-	getControllers: function(req, res) {
-		return res.json(utility.getAllControllers())
-	},
-
-	whatResources: function(req, res) {
-		var roles = safeLowerCase(req.param('roles'))
-
-		var paramsError = utility.checkParams(req, 'roles')
-
-		if (paramsError) {
-			return res.badRequest(paramsError)
-		}
-
-		acl.whatResources(roles, function(err, result) {
-			if (err) {
-				winston.error(err)
-				return res.serverError(err)
-			}
-			else {
-				return res.json(result)
-			}
-		})
+		});
 
 	},
 
-  // use this function to test ACL
-  test: function(req, res) {
-    return res.ok()
-  }
+	// use this function to test ACL
+	test: function (req, res) {
+		return res.ok();
+	}
 }
 
 
 // Convert a string or array of strings to lower case
-var safeLowerCase = function(input) {
+var safeLowerCase = function (input) {
 	if (_.isString(input)) {
-		return input.toLowerCase()
+		return input.toLowerCase();
 	}
 	if (_.isArray(input)) {
-		return _.map(input, function(s) {
-			return s.toLowerCase()
+		return _.map(input, function (s) {
+			return s.toLowerCase();
 		})
 	}
 }
