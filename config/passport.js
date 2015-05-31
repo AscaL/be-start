584a55635b126851e54ce12faa812e085731f470
AscaL <emanuele.orfanelli@fwr.it>
2015-05-31 03:19:49 +0200

    upd travis and jwt fields

--- config/passport.js
+++ config/passport.js
@@ -13,76 +13,76 @@ var SECRET = process.env.tokenSecret || '4ukI0uIVnB3iI1yxj646fVXSE3ZVk4doZgz6fTb
 
 // Use a different secret when running tests
 if (process.env.NODE_ENV === 'test')
-  SECRET = 'AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA';
+	SECRET = 'AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA';
 
 //NOTE: This token is gonna be used for testing purposes.
 var TESTTOKEN = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyIjp7InVzZXJuYW1lIjoidGVzdDEiLCJlbWFpbCI6InRlc3QxQHRlc3QuY29tIiwiZmlyc3ROYW1lIjoiIiwibGFzdE5hbWUiOiIiLCJjcmVhdGVkQXQiOiIyMDE1LTA1LTI4VDE2OjAzOjM2LjY1NVoiLCJ1cGRhdGVkQXQiOiIyMDE1LTA1LTI4VDE2OjAzOjM2LjY1NVoiLCJpZCI6M30sImlhdCI6MTQzMjgyOTAxNiwiYXVkIjoiZndyLml0IiwiaXNzIjoiZndyLml0In0.4Y2W_urcN_wIkuB8yxz0hfETSsY1_2VjSJThYg88Jmw'
 
 var ALGORITHM = 'HS256';
-var ISSUER = 'ascal.it';
-var AUDIENCE = 'ascal.it';
+var ISSUER = 'fwr.it';
+var AUDIENCE = 'fwr.it';
 
 /**
  * Configuration object for local strategy
  */
 var LOCAL_STRATEGY_CONFIG = {
-  usernameField: 'email',
-  passwordField: 'password',
-  passReqToCallback: false
+	usernameField: 'email',
+	passwordField: 'password',
+	passReqToCallback: false
 };
 
 /**
  * Configuration object for JWT strategy
  */
 var JWT_STRATEGY_CONFIG = {
-  secretOrKey: SECRET,
-  issuer: ISSUER,
-  audience: AUDIENCE,
-  passReqToCallback: false
+	secretOrKey: SECRET,
+	issuer: ISSUER,
+	audience: AUDIENCE,
+	passReqToCallback: false
 };
 
 /**
  * Triggers when user authenticates via local strategy
  */
 function _onLocalStrategyAuth(email, password, next) {
-  User.findOne({
-	  email: email
-	})
-	.exec(function (error, user) {
-	  if (error) return next(error, false, {});
+	User.findOne({
+			email: email
+		})
+		.exec(function (error, user) {
+			if (error) return next(error, false, {});
 
-	  if (!user) return next(null, false, {
-		code: 'E_USER_NOT_FOUND',
-		message: email + ' is not found'
-	  });
+			if (!user) return next(null, false, {
+				code: 'E_USER_NOT_FOUND',
+				message: email + ' is not found'
+			});
 
-	  // TODO: replace with new cipher service type
-	  if (!cipherService.comparePassword(password, user))
-		return next(null, false, {
-		  code: 'E_WRONG_PASSWORD',
-		  message: 'Password is wrong'
+			// TODO: replace with new cipher service type
+			if (!cipherService.comparePassword(password, user))
+				return next(null, false, {
+					code: 'E_WRONG_PASSWORD',
+					message: 'Password is wrong'
+				});
+
+			return next(null, user, {});
 		});
-
-	  return next(null, user, {});
-	});
 }
 
 /**
  * Triggers when user authenticates via JWT strategy
  */
 function _onJwtStrategyAuth(payload, next) {
-  var user = payload.user;
-  return next(null, user, {});
+	var user = payload.user;
+	return next(null, user, {});
 }
 
 passport.use(new LocalStrategy(LOCAL_STRATEGY_CONFIG, _onLocalStrategyAuth));
 passport.use(new JwtStrategy(JWT_STRATEGY_CONFIG, _onJwtStrategyAuth));
 
 module.exports.jwtSettings = {
-  expiresInMinutes: EXPIRES_IN_MINUTES,
-  secret: SECRET,
-  algorithm: ALGORITHM,
-  issuer: ISSUER,
-  audience: AUDIENCE,
-  testToken: TESTTOKEN
+	expiresInMinutes: EXPIRES_IN_MINUTES,
+	secret: SECRET,
+	algorithm: ALGORITHM,
+	issuer: ISSUER,
+	audience: AUDIENCE,
+	testToken: TESTTOKEN
 };
