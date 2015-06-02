82a9c8488699286941385766bc2b6d0f6cae8f9c
AscaL <emanuele.orfanelli@fwr.it>
2015-06-02 03:27:55 +0200

    remove gulp coveralls task

--- gulpfile.js
+++ gulpfile.js
@@ -4,56 +4,50 @@ var stylish = require('jshint-stylish');
 var size = require('gulp-size');
 var todo = require('gulp-todo');
 var notify = require('gulp-notify');
-var coveralls = require('gulp-coveralls');
 var del = require('del');
 
 //TODO: define the folders better
 
 gulp.task('default', function () {
-	// place code for your default task here
+  // place code for your default task here
 });
 
 gulp.task('clean', function (cb) {
-	del([
-		'node_modules',
-		'coverage'
-	], cb);
+  del([
+    'node_modules',
+    'coverage'
+  ], cb);
 });
 
 gulp.task('lint', function () {
-	return gulp.src(['./api/**/*.js', './config/**/*.js', './docs/**/*.js', './test/**/*.js'])
-		.pipe(jshint())
-		.pipe(jshint.reporter(stylish));
+  return gulp.src(['./api/**/*.js', './config/**/*.js', './docs/**/*.js', './test/**/*.js'])
+    .pipe(jshint())
+    .pipe(jshint.reporter(stylish));
 });
 
 gulp.task('size', function () {
-	var s = size({
-		showFiles: true
-	});
-	return gulp.src(['./api/**/*.js', './config/**/*.js', './docs/**/*.js', './test/**/*.js'])
-		.pipe(s)
-		.pipe(notify({
-			onLast: true,
-			message: function () {
-				return 'Total size ' + s.prettySize;
-			}
-		}));
+  var s = size({
+    showFiles: true
+  });
+  return gulp.src(['./api/**/*.js', './config/**/*.js', './docs/**/*.js', './test/**/*.js'])
+    .pipe(s)
+    .pipe(notify({
+      onLast: true,
+      message: function () {
+        return 'Total size ' + s.prettySize;
+      }
+    }));
 });
 
 gulp.task('todo', function () {
-	gulp.src(['./api/**/*.js', './config/**/*.js', './docs/**/*.js', './test/**/*.js'])
-		.pipe(todo({
-			absolute: true
-		}))
-		.pipe(gulp.dest('./'))
-		.pipe(todo.reporter('json', {
-			fileName: 'todo.json',
-			absolute: true
-		}))
-		.pipe(gulp.dest('./'))
-});
-
-gulp.task('coveralls', function () {
-	gulp.src('./coverage/lcov.info')
-		.pipe(coveralls());
+  gulp.src(['./api/**/*.js', './config/**/*.js', './docs/**/*.js', './test/**/*.js'])
+    .pipe(todo({
+      absolute: true
+    }))
+    .pipe(gulp.dest('./'))
+    .pipe(todo.reporter('json', {
+      fileName: 'todo.json',
+      absolute: true
+    }))
+    .pipe(gulp.dest('./'))
 });
